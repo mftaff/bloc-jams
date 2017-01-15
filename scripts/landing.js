7a5c8dd9e1f614d13c4fde553b039b2b6bf667f4
Meir Taffel <artgardening@gmail.com>
2017-01-15 13:33:36 +0200

    Complete checkpoint 9. add scroll trigger for animations. remove custom settings for animation.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
     
     var revealPoint = function(loopCount) {
         for (var i = 0; i < loopCount; i++) {
@@ -12,3 +12,19 @@ var animatePoints = function() {
     };
     revealPoint(points.length);
 };
+
+window.onload = function() {
+    
+    if (window.innerheight > 950) {
+        animatePoints(pointsArray);  
+    };
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);      
+        };
+    });
+}
