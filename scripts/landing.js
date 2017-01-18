5771e440826edb9c394df51c880ce2e0346ed476
Meir Taffel <artgardening@gmail.com>
2017-01-18 11:48:03 +0200

    Complete: checkpoint 16. add jquery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,23 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-    
-    var revealPoint = function(loopCount) {
-        for (var i = 0; i < loopCount; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";    
-        }  
+var animatePoints = function() {   
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });    
     };
-    revealPoint(points.length);
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
+$(window).load(function () {     
+    if ($(window)height() > 950) {
+        animatePoints();  
+    }
     
-    if (window.innerheight > 950) {
-        animatePoints(pointsArray);  
-    };
-    
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.sellingPoints').offset().top - $(window)Height() + 200;
 
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);      
-        };
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
+        }
     });
-}
+});
